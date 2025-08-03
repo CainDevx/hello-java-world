460f98bae9303e832f56e2a0af4e78d79bf9c8d5
Cain.Dev <cain.devx@gmail.com>
2025-08-03 00:02:27 -0300

    Actualizando repo

---
+++ Inicios/EjerciciosMios.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+
+public class EjerciciosMios {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        //ver porque tengo que crear el objeto y que es un objeto
+
+
+        System.out.println("Operacion a Realizar");
+        String operacion = scanner.nextLine();
+
+        System.out.println(operacion);
+
+
+
+
+
+
+    }
+}
